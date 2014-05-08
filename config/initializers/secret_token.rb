fd7673e6d52a8dbf2fc9b5dbe37e5fe5bc28af38
Topias Heinonen <corvus@iki.fi>
2014-05-08 20:50:03 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PetulantBatman::Application.config.secret_key_base = '2635458dfc76a74c157f92cd1096d75312168755ec3593899216bb5c35e34747917c83c13285fd036050ff9ce1404bd2a8097a86f289f51f0c1763fbf62e1ed4'
