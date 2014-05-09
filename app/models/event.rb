90374ca823dd971972b8a6ff28faec526b789654
Topias Heinonen <corvus@iki.fi>
2014-05-09 23:45:33 +0300

    Fix for timezones

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,17 @@ require 'icalendar'
 class Event < ActiveRecord::Base
   belongs_to :event_list
 
+
+
   def to_ical
+    event_start = DateTime.parse(start)
+    event_end = DateTime.parse(start) + length.minutes
+
+    tzid = "Europe/Helsinki"
+
     e = Icalendar::Event.new
-    e.dtstart = DateTime.parse(start)
-    e.dtend = DateTime.parse(start) + length.minutes
+    e.dtstart = Icalendar::Values::DateTime.new event_start, 'tzid' => tzid
+    e.dtend = Icalendar::Values::DateTime.new event_end, 'tzid' => tzid
     e.location = place
     e.summary = name
     e
